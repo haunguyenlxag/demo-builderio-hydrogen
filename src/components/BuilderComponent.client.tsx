5300d224107a47b5820244bb541964afcbc319b5
Hau Nguyen <hau.nguyen@merkle.com>
2023-02-06 12:31:22 +0700

    init package

---
+++ src/components/BuilderComponent.client.tsx
@@ -0,0 +1,6 @@
+import {builder, BuilderComponent} from '@builder.io/react';
+import "@builder.io/widgets";
+
+builder.init('5e5763d7cc284548bffa393de98e52c1');
+
+export { BuilderComponent };
---
+++ src/routes/components/BuilderComponent.client.tsx
@@ -0,0 +1,13 @@
+// Do stuff here
+import {BuilderComponent, builder, Builder} from '@builder.io/react';
+
+builder.init('5e5763d7cc284548bffa393de98e52c1');
+
+// Add custom components
+//
+// Builder.registerComponent(Welcome, {
+//   name: 'Welcome',
+// });
+
+
+export default BuilderComponent;
